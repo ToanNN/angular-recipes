b01dd16870957f77be068d4d0a90a80fb8a242f8
Toan Nguyen <toan.1aus@gmail.com>
2022-01-08 21:38:35 +1000

    Generate reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormValidationsComponent } from './reactive-form/form-validations/form-validations.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormValidationsComponent
   ],
   imports: [
     BrowserModule,
