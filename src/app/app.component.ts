c5e19dbe2b7400bb102065aadc2491c2e3d00061
Toan Nguyen <toan.1aus@gmail.com>
2022-01-15 15:26:00 +1000

    Add menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NavigationCancel, NavigationEnd, NavigationError, Event, NavigationStart, Router } from '@angular/router';
+import { MessageService } from './message/message.service';
+import { AuthService } from './security/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularRecipes';
+  title = 'Angular Recipes';
+  loading = true;
+
+  get isLoggedIn(): boolean {
+    return this.authService.isLoggedIn;
+  }
+
+  get isMessageDisplayed(): boolean {
+    return this.messageService.isDisplayed;
+  }
+
+  get userName(): string {
+    if (this.authService.currentUser) {
+      return this.authService.currentUser.userName;
+    }
+    return '';
+  }
+  constructor(private authService: AuthService,
+    private router: Router,
+    private messageService: MessageService) {
+    router.events.subscribe(this.checkRouterEvent)
+  }
+
+  checkRouterEvent(routerEvent: Event): void {
+    if (routerEvent instanceof NavigationStart) {
+      this.loading = true;
+    }
+
+    if (routerEvent instanceof NavigationEnd ||
+      routerEvent instanceof NavigationCancel ||
+      routerEvent instanceof NavigationError) {
+      this.loading = false;
+    }
+  }
+
+  displayMessages(): void {
+    this.router.navigate([{ outlets: { popup: ['messages'] } }]);
+    this.messageService.isDisplayed = true;
+  }
+
+  hideMessages(): void {
+    this.router.navigate([{ outlets: { popup: null } }]);
+    this.messageService.isDisplayed = false;
+  }
+
+  logOut(): void {
+    this.authService.logOut();
+    this.router.navigateByUrl('/welcome');
+  }
+
 }
