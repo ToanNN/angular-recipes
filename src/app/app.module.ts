44115e7f1348e945d297865181698b9a5001a78a
Toan Nguyen <toan.1aus@gmail.com>
2022-01-15 17:23:46 +1000

    Add routes to products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MessageModule } from './message/message/message.module';
 import { AppReactiveFormModule } from './reactive-form/reactive-form-module/app-reactive-form.module';
+import { AuthGuard } from './security/auth.guard';
 import { SecurityModule } from './security/security.module';
 
 
@@ -17,7 +19,15 @@ import { SecurityModule } from './security/security.module';
     AppRoutingModule,
     AppReactiveFormModule,
     MessageModule,
-    SecurityModule
+    SecurityModule,
+    RouterModule.forRoot([
+      {
+        path: 'products',
+        canActivate: [AuthGuard],
+        data: { preload: false },
+        loadChildren: () => import('./products/products.module').then(m => m.ProductsModule)
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,6 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductService } from './product.service';
+import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
+import { ProductResolverService } from './product-resolver.service';
+import { ProductEditGuard } from './product-edit.guard';
+import { ProductEditInfoComponent } from './product-edit-info/product-edit-info.component';
+import { ProductEditTagsComponent } from './product-edit-tags/product-edit-tags.component';
 
 
 
@@ -10,10 +17,38 @@ import { ProductEditComponent } from './product-edit/product-edit.component';
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ProductEditComponent
+    ProductEditComponent,
+    ProductEditInfoComponent,
+    ProductEditTagsComponent
   ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    SharedModule,
+    RouterModule.forChild(
+      [
+        {
+          path: '',
+          component: ProductListComponent
+        },
+        {
+          path: ':id',
+          component: ProductDetailComponent,
+          resolve: { resolvedData: ProductResolverService }
+        },
+        {
+          path: ':id/edit',
+          component: ProductEditComponent,
+          canDeactivate: [ProductEditGuard],
+          resolve: { resolvedData: ProductResolverService },
+          children: [
+            { path: '', redirectTo: 'info', pathMatch: 'full' },
+            { path: 'info', component: ProductEditInfoComponent },
+            { path: 'tags', component: ProductEditTagsComponent }
+          ]
+        }
+      ]
+    )
+  ],
+  providers: [ProductService]
 })
 export class ProductsModule { }
